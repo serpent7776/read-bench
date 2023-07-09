aba792355a4e9c1ed687c85a72104dee2e35f2d8
Serpent7776 <Serpent7776@gmail.com>
2023-07-09 11:59:08 +0200

    Made scv script shorter

--- csv.sh
+++ csv.sh
@@ -4,23 +4,15 @@
 use strict;
 use warnings;
 use feature q/say/;
-my ($user, $system, $elapsed, $cpu, $maxresident, $major, $minor);
 $,=", ";
 while (<>) {
 	if (m#^time\s./(.*)$#) {
 		my $label="$1";
-		my $times = <>;
-		$times =~ m#([\d.]+)user\s+([\d.]+)system\s+([\d.:]+)elapsed\s+(\d+)%CPU.+\d*avgtext.*\d*avgdata\s+(\d+)maxresident.k#;
-		$user=$1;
-		$system=$2;
-		$elapsed=$3;
-		$cpu=$4;
-		$maxresident=$5;
-		my $ios = <>;
-		$ios =~ m#\d+inputs.*\d+outputs.*(\d+)major.(\d+)minor.*pagefaults\s*\d+swaps#;
-		$major=$1;
-		$minor=$2;
-		say $label, $user, $system, $elapsed, $cpu, $maxresident, $major, $minor;
+		my $t = <>;
+		my @times = ($t =~ m#([\d.]+)user\s+([\d.]+)system\s+([\d.:]+)elapsed\s+(\d+)%CPU.+\d*avgtext.*\d*avgdata\s+(\d+)maxresident.k#);
+		my $i = <>;
+		my @ios = ($i =~ m#\d+inputs.*\d+outputs.*(\d+)major.(\d+)minor.*pagefaults\s*\d+swaps#);
+		say $label, @times, @ios;
 	}
 }
 '
